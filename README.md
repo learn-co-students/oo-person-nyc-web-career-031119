3005b5c95e70c8c1b078f1ba7def3bb0b494f635
Theo Weller <weller.theodor@gmail.com>
2019-03-13 17:58:19 +0000

    Done.

--- README.md
+++ README.md
@@ -17,10 +17,10 @@ To practice object oriented programming (OOP), you're going to create a Person c
 
 
 ## Instructions
-- First you need to create a person class that is initialized with a name that cannot be changed.
-- Each instance of class `Person`should be able to remember their name
-- Each instance of  class `Person` should start with $25 in their bank accounts
-- Each instance of  class `Person` should start with eight happiness points
+- First you need to create a person class that is initialized with a name that cannot be changed. #CONSTANTS
+- Each instance of class `Person`should be able to remember their name #SETTER
+- Each instance of  class `Person` should start with $25 in their bank accounts #initialize
+- Each instance of  class `Person` should start with eight happiness points #
 - Each instance of  class `Person` should start with eight hygiene points
 - The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and  0 respectively
 - The amount in the bank account should also be able to change, though it has no max or min.
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
-# your code goes here
+require 'pry'
+class Person
+#attr_writer
+attr_accessor :hygiene, :bank_account, :happiness
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+end
+
+def happiness=(happiness)
+  @happiness = happiness
+  if @happiness > 10
+    @happiness = 10
+  elsif @happiness < 0
+    @happiness = 0
+  end
+  end
+
+def hygiene=(hygiene)
+  @hygiene = hygiene
+  if @hygiene > 10
+    @hygiene = 10
+  elsif @hygiene < 0
+    @hygiene = 0
+end
+end
+
+def happy?
+return true if @happiness > 7
+false
+end
+
+def clean?
+return true if @hygiene > 7
+false
+end
+
+def get_paid(salary)
+  @bank_account += salary
+  return "all about the benjamins"
+end
+
+def take_bath
+  self.hygiene=(hygiene + 4)
+  return "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+
+def work_out
+  self.hygiene=(hygiene - 3)
+    self.happiness=(happiness + 2)
+  return '♪ another one bites the dust ♫'
+end
+
+def call_friend(friend)
+friend.happiness=(friend.happiness + 3)
+self.happiness=(happiness + 3)
+return "Hi #{friend.name}! It's #{self.name}. How are you?"
+end
+
+def start_conversation(person, topic)
+
+case topic
+
+when topic = "politics"
+  person.happiness=(person.happiness - 2)
+  self.happiness=(happiness - 2)
+  "blah blah partisan blah lobbyist"
+
+when topic = "weather"
+  self.happiness=(happiness + 1)
+  person.happiness=(person.happiness + 1)
+  "blah blah sun blah rain"
+
+else
+  "blah blah blah blah blah"
+end
+end
+end
+
+# person = new Person('vera')
+# person2 = new Person('fred')
+# person.call_friend(person2)
