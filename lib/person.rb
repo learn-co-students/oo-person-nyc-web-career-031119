6712dfd662488eb98e6aedcb69e8da0d41b558b8
github username <github email address>
2019-04-25 17:46:19 -0400

    m

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Person
-attr_reader :name, :happiness, :hygiene
-attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
 
   def initialize(name)
     @name = name
@@ -9,23 +9,22 @@ attr_accessor :bank_account
     @happiness = 8
     @hygiene = 8
   end
+
   def happiness=(happiness)
     if happiness >= 10
       @happiness = 10
     elsif happiness <= 0
       @happiness = 0
-    else
-      @happiness = happiness
+    else @happiness = happiness
     end
   end
 
   def hygiene=(hygiene)
     if hygiene >= 10
-     @hygiene = 10
+      @hygiene = 10
     elsif hygiene <= 0
       @hygiene = 0
-    else
-      @hygiene = hygiene
+    else @hygiene = hygiene
     end
   end
 
@@ -38,41 +37,43 @@ attr_accessor :bank_account
   end
 
   def get_paid(salary)
-    @bank_account += salary
-    'all about the benjamins'
-  end
+   @bank_account += salary
+   "all about the benjamins"
+ end
 
   def take_bath
     self.hygiene += 4
-    '♪ Rub-a-dub just relaxing in the tub ♫'
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
     self.hygiene -= 3
     self.happiness += 2
-    '♪ another one bites the dust ♫'
+    "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
-    Person.new(friend)
     self.happiness += 3
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-  def start_conversation(friend,topic)
-    Person.new(friend)
+  def start_conversation(person, topic)
+    # binding.pry
     if topic == "politics"
       self.happiness -= 2
-      friend.happiness -= 2
-      "blah blah partisan blah lobbyist"
+      person.happiness -= 2
+      'blah blah partisan blah lobbyist'
     elsif topic == "weather"
       self.happiness += 1
-      friend.happiness += 1
-      "blah blah sun blah rain"
-    else topic == 'other'
+      person.happiness += 1
+      'blah blah sun blah rain'
+    else
       'blah blah blah blah blah'
     end
   end
+
+
+
 
 end
